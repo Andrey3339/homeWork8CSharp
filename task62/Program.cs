7dc44f1cbebc083e1e3ae195a14974a817869a1f
Andrey Shevchuk <Shevchuk339@mail.ru>
2023-03-01 22:54:29 +0300

    add task62

---
+++ task62/Program.cs
@@ -0,0 +1,40 @@
+﻿
+void InputMatrix(int[,] matrix)
+{
+  int number = 1;
+  int x = 0;
+  int y = 0;
+  int size = matrix.GetLength(0);
+  for (int i = 0; i < size * size; i++)
+  {	
+    matrix[x, y] = number;
+    if(x <= y + 1 && x + y < size - 1)
+      y++;
+    else if (x < y && x + y >= size -1)
+      x++;
+    else if (x >= y && x + y > size - 1)
+      y--;
+    else
+      x--;
+    number++;
+  }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+  for (int i = 0; i < matrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+      Console.Write($"{matrix[i, j].ToString("00")} \t");
+    }
+    Console.WriteLine();
+  }
+}
+  
+
+Console.Clear();
+Console.WriteLine("Полученный массив 4 на 4: "); 
+int[,] matrix = new int[4, 4];
+InputMatrix(matrix);
+PrintMatrix(matrix);
